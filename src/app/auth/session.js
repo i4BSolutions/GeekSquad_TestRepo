0631162e18b1788118493ebb704bc895e553324e
AyeNyeinSan <aye2904@gmail.com>
2025-01-11 16:16:41 +0700

    Delete: delete unused functions and db integration

--- src/app/auth/session.js
+++ src/app/auth/session.js
@@ -1,27 +1,9 @@
 import { NextResponse } from 'next/server';
 import db from '../utils/db';
 import jwt from 'jsonwebtoken';
-import { SignJWT, jwtVerify } from 'jose';
 
-export async function encrypt(payload) {
-  return new SignJWT(payload)
-    .setProtectedHeader({ alg: "HS256" })
-    .setIssuedAt()
-    .setExpirationTime("1hr")
-    .sign(key);
-}
+const JWT_SECRET= "mysecret";
 
-export async function decrypt(session) {
-  try {
-    const { payload } = await jwtVerify(session, key, {
-      algorithms: ["HS256"],
-    });
-    return payload;
-  } catch (error) {
-    console.log("Failed to verify session");
-    return null;
-  }
-}
 //create session
 export async function createSession(user) {
  
@@ -31,10 +13,7 @@ const token = jwt.sign(
   { expiresIn: "1d" }
 );
 console.log("Token: ", token);
-  // Update the UserToken for the specified UserId
-  await db("UPDATE User SET UserToken = ? WHERE UserId = ?", [token, user.UserId]);
-    return token;
-
+ 
 }
 
 
@@ -45,7 +24,7 @@ export async function verifySession(token) {
     
     return null;}
   try {
-    const decoded =jwt.verify(token, process.env.JWT_SECRET);
+    const decoded =jwt.verify(token, JWT_SECRET);
     return decoded;
   } catch (error) {
     console.error("JWT verification failed:", error.message);
