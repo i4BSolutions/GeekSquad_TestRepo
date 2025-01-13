b98f8fcbd67105dacd1bdea4e864580fcfea563c
AyeNyeinSan <aye2904@gmail.com>
2025-01-13 12:48:17 +0700

    Refactor: dashboard page test case

--- src/app/dashboard/__tests__/dashboardPage.test.js
+++ src/app/dashboard/__tests__/dashboardPage.test.js
@@ -10,7 +10,17 @@ describe("DashboardPage", () => {
   afterEach(() => {
     jest.clearAllMocks(); // Clear mocks after each test
   });
-  
+  it("include Home text in dashboard Page", async () => {
+    
+    //Arrange 
+    render(<DashboardPage />);
+    //Act
+    //verify heading text
+    const heading = screen.getByRole("heading", { name: /home/i });
+
+    //Assert
+   expect(heading).toBeInTheDocument();
+  });
 
 
 });
