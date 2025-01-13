a320d21a58016f35e0dbec86f7fa8765f2bd9a6d
AyeNyeinSan <aye2904@gmail.com>
2025-01-13 13:07:13 +0700

    Update: add alert func: for welcome msg

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,11 +1,14 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./dashboard.css";
 import Cart from "../cart/page";
+
 export default function DashboardPage() {
   const [cart, setCart] = useState([]);
-
   const [showCart, setShowCart] = useState(false);
+  const [user, setUser] = useState(null);
+
+
 
   const toggleCart = () => {
     setShowCart(!showCart);
@@ -26,6 +29,39 @@ export default function DashboardPage() {
     setCart((prevCart) => [...prevCart, product]);
   };
 
+  // Fetch user data and set welcome message
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const response = await fetch("/api/auth/login", {
+          method: "GET",
+          credentials: "include", // Include cookies for authentication
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setUser(data); // Save user data
+          if (data?.email) {
+            alert(`Welcome to the Dashboard, ${data.email}!`);
+          }
+         
+        } else {
+          console.error("Failed to fetch user data");
+          setWelcomeMsg("Welcome to the Dashboard!");
+        }
+        // Show message for 3 seconds
+        setShowWelcome(true);
+        setTimeout(() => {
+          setShowWelcome(false);
+        }, 3000);
+      } catch (error) {
+        console.error("Error fetching user:", error);
+        setWelcomeMsg("Welcome to the Dashboard!");
+      }
+    };
+    fetchUser();
+  }, []);
+
   return (
     <div>
       {!showCart && (
@@ -41,6 +77,7 @@ export default function DashboardPage() {
             </div>
           </header>
           <main className="products-container">
+           
             {products.map((product) => (
               <div key={product.id} className="product-card">
                 <div className="product-image"></div>
