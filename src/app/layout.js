13ea83914c32c7801cd1cbee176815ddf1f9e56f
AyeNyeinSan <aye2904@gmail.com>
2025-01-24 20:59:54 +0700

    Add: use context provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import { UserProvider } from "./context/UserContext";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -20,7 +22,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <UserProvider>{children}</UserProvider>
       </body>
     </html>
   );
