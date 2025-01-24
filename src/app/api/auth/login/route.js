9fe6aebbcd3846ec2fd50ffabac363a878198255
AyeNyeinSan <aye2904@gmail.com>
2025-01-24 18:09:46 +0700

    Update: get user from DB using decoded email

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,7 +1,7 @@
 import { login
  } from "@/app/auth/auth"; 
 import { NextResponse } from "next/server";
-
+import supabase from "@/app/utils/supabase";
 import { verifySession } from "../../../auth/session";
 
 export async function POST(req, res) {
@@ -24,10 +24,34 @@ export async function GET(req) {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
    
+      //Query Database to get user data 
 
-  
+  const {data , error} = await supabase
+  .from('Users')
+  .select('Name,Email,PhoneNumber,Address')
+  .eq('Email',decoded.email)
+  .single();
+
+  console.log("Data from supabase: ", data);
+ if (error) {
+   console.error("Error fetching user data:", error.message);
+   return NextResponse.json(
+     { error: "Failed to fetch user data" },
+     { status: 500 }
+   );
+ }
+
+ if (!data) {
+   return NextResponse.json({ error: "User not found" }, { status: 404 });
+ }
+
     return NextResponse.json({
       email: decoded.email,
+      fullName: data.Name ,
+      email: data.Email,
+      phoneNumber: data.PhoneNumber,
+      address : data.Address
+
     });
   } catch (error) {
     console.error("Error fetching user:", error);
